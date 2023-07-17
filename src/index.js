e6aa5fdf4739aef24d271a077000046dfc53ace6
ihorkoi <koleev.igor@gmail.com>
2023-07-17 17:57:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const axios = require('axios').default;
+// const axios = require('axios').default;
 import Notiflix from 'notiflix';
 
 import simplelightbox from "simplelightbox";
